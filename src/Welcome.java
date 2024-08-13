5c9d1377d42b347d5cdf420b6e12009ee8dc7f4d
VienThieu25 <23658881.thieu@student.iuh.edu.vn>
2024-08-13 12:00:45 +0700

    message

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public record Welcome() {
+    public static void main(String[] args) {
+        System.out.println("Hello Tram Hong Vien Thieu");
+        System.out.println("Welcome to the DHKTPM19A");
+    }
+}
